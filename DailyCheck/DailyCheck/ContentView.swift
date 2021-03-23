b1296c056c41bd7c028222cd646384a41e2edd0f
taemin8712 <taemin8712@gmail.com>
2021-03-22 19:22:59 -0700

    DaliyCheck Sample

---
+++ DailyCheck/DailyCheck/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DailyCheck
+//
+//  Created by taemmy on 2020/07/14.
+//  Copyright © 2020 tm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
